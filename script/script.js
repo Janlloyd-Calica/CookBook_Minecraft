ef568fce71a242b7d1fc9c3254b9b08f0644184a
Jan lloyd <janlloydcalica@gmial.com>
2024-02-20 18:04:46 +0800

    java

---
+++ script/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Simulating a loading process with setTimeout
+    setTimeout(function () {
+        
+        window.location.href = 'main.html';
+    }, 13000);
+});
