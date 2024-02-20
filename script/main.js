42f599d1b43c72fbad9bffe645790a51cf8f4c5e
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-02-20 21:43:20 +0800

    Fixing next html

---
+++ script/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var bigDiv = document.querySelector('.big');
+    bigDiv.addEventListener('click', function() {
+        window.location.href = 'craft.html';
+    });
+});
