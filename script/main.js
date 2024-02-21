28b3605e5f480f97f3df7dd29126dc05a4543a7e
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-02-21 21:17:47 +0800

    Added craft.html button

--- script/main.js
+++ script/main.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var bigDiv = document.querySelector('div.big');
-    bigDiv.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Cook Book" title element
+    var cookBookTitle = document.querySelector('.title');
+
+    // Add a click event listener to the "Cook Book" title
+    cookBookTitle.addEventListener('click', function() {
+        // Redirect to the specified HTML file
         window.location.href = 'craft.html';
     });
 });
