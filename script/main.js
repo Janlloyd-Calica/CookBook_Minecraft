f9de2ff2ace89a2ad3c260506c0f899f5274de83
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-02-21 21:17:05 +0800

    Fixed html next page

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var bigDiv = document.querySelector('.big');
+    var bigDiv = document.querySelector('div.big');
     bigDiv.addEventListener('click', function() {
         window.location.href = 'craft.html';
     });
